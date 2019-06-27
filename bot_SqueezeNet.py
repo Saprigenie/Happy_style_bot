b5582c216368931e21b65f95b9bfbc666b1cbe85
Saprigenie <51880532+Saprigenie@users.noreply.github.com>
2019-06-27 15:16:54 +0200

    Update bot_SqueezeNet.py

--- bot_SqueezeNet.py
+++ bot_SqueezeNet.py
@@ -13,7 +13,7 @@ from bot_utils.telegram_token import token_my
 #Импортируем нейросеточку для переноса стиля с любой картинки(но в плохом качестве + медленно)
 from bot_utils.neural_style_with_your_image_SqueezeNet import run_style_transfer_with_your_image
 # Импортируем файл, в котором все ответы бота
-import bot_utils.config
+import bot_utils.config_SqueezeNet
 
 # Эта переменная необходима, чтобы запомнить выбор стиля пользователя
 answer = str()
@@ -22,8 +22,8 @@ first_image_file = {}
 def fun(bot, update):
     global answer
     answer = ''
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.fun_1 )
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.fun_2)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.fun_1 )
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.fun_2)
     
 def send_prediction(bot, update):
     # Нам нужно получить две картинки, чтобы произвести перенос стиля, но каждая картинка приходит в
@@ -46,50 +46,50 @@ def send_prediction(bot, update):
         style_image_stream = BytesIO()
         image_file.download(out=style_image_stream)
         
-        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.fun_go)
+        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.fun_go)
         output = run_style_transfer_with_your_image(bot, update, content_image_stream, style_image_stream)
-        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.fun_done)
+        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.fun_done)
     else:
         first_image_file[chat_id] = image_file
-        bot.send_message(chat_id=chat_id, text=bot_utils.config.fun_3)
+        bot.send_message(chat_id=chat_id, text=bot_utils.config_SqueezeNet.fun_3)
     
 # Команда, с которой бот будет нас приветствовать
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.startCommand_1)
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.startCommand_2)
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.startCommand_3)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.startCommand_1)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.startCommand_2)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.startCommand_3)
 
 # Введя эту команду, пользователь получит возможность посмотреть все возможные стили для переноса
 def style(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_0_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_0_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_0_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_0_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_1_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_1_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_1_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_1_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_2_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_2_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_2_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_2_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_3_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_3_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_3_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_3_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_4_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_4_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_4_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_4_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_5_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_5_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_5_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_5_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_6_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_6_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_6_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_6_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_7_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_7_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_7_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_7_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_8_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_8_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_8_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_8_photo)
     
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.style_9_text)
-    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.style_9_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.style_9_text)
+    bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.style_9_photo)
     
     
 # Эта функция будет проверять каждое текстовое сообщение пользователя, цифра это в нужном диапозоне или нет.
@@ -98,15 +98,15 @@ def text(bot, update):
     answer = update.message.text
     if answer=='1' or answer=='2' or answer=='3' or answer=='4' or answer=='5' or answer=='6' or answer=='7' or answer=='8':
         answer = int(answer)
-        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.answer_1_8)
+        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.answer_1_8)
     elif answer=='9':
         answer = int(answer)
-        bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config.photo_9, caption=bot_utils.config.answer_9)
+        bot.send_photo(chat_id=update.message.chat_id, photo=bot_utils.config_SqueezeNet.photo_9, caption=bot_utils.config_SqueezeNet.answer_9)
     elif answer=='0':
         answer = random.randint(1, 9)
-        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.answer_0_1 + str(answer) + bot_utils.config.answer_0_2)
+        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.answer_0_1 + str(answer) + bot_utils.config_SqueezeNet.answer_0_2)
     else:
-        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.answer_other)
+        bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.answer_other)
     print(answer)
     return answer
   
@@ -114,7 +114,6 @@ def text(bot, update):
 def send_stylized_image(bot, update):
     # Мы получаем картинку контента и должны использовать выбранный пользователем стиль
     chat_id = update.message.chat_id
-    print(chat_id)
     print("Got image from {}".format(chat_id))
     style = ['candy.pth','mosaic.pth','rain_princess.pth','udnie.pth', 'AbstractPicasso.model', 'Whist.model', 'Futurism.model', 'Anime.model', 'Error.model']
     # получаем информацию о картинке
@@ -124,7 +123,7 @@ def send_stylized_image(bot, update):
     # Подаем картинку нашей нейросети
     content_image_stream = BytesIO()
     image.download(out=content_image_stream)
-    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config.send_magic_photo)
+    bot.send_message(chat_id=update.message.chat_id, text=bot_utils.config_SqueezeNet.send_magic_photo)
     output = stylize(content_image_stream, 'models/'+ style[answer - 1])
     
     # теперь отправим назад фото
